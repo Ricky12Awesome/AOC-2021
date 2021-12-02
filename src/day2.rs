f4a9c47669693a425d23581a479e7590cc263a61
Ricky12Awesome <rickygraveen@gmail.com>
2021-12-01 23:16:26 -0600

    Day2 (very messy)

--- src/day2.rs
+++ src/day2.rs
@@ -4,8 +4,49 @@ day!(Day2);
 
 impl Day for Day2 {
   fn day(part: Part) -> Answer {
-    let part1 = || "Unimplemented";
-    let part2 = || "Unimplemented";
+    let lines = Self::INPUT
+      .lines()
+      .map(|it| {
+        let (pos, amount) = it.split_once(" ").unwrap();
+        let amount = amount.parse::<i32>().unwrap();
+
+        match pos {
+          "forward" => [amount, 0],
+          "down" => [0, amount],
+          "up" => [0, -amount],
+          _ => [0, 0]
+        }
+      });
+
+    let lines = lines.collect::<Vec<_>>();
+
+    let part1 = || {
+      let mut hor = 0;
+      let mut dep = 0;
+
+      for [x, y] in lines.clone() {
+        hor += x;
+        dep += y;
+      }
+
+      hor * dep
+    };
+
+
+    let lines = lines.clone();
+    let part2 = || {
+      let mut hor = 0;
+      let mut dep = 0;
+      let mut aim = 0;
+
+      for [x, y] in lines {
+        hor += x;
+        aim += y;
+        dep += x * aim;
+      }
+
+      hor * dep
+    };
 
     answer!(part, part1, part2)
   }
