3cdcdf7bcc3704977f10ee7868ac0ec54d35cc31
Ricky12Awesome <rickygraveen@gmail.com>
2021-12-04 09:51:06 -0600

    Day4 Improved (working)

--- src/day4.rs
+++ src/day4.rs
@@ -10,41 +10,46 @@ impl Day4 {
       .map(|it| it.split(' ').filter(not_empty).map(parse::<u32>).collect_vec())
       .collect_vec();
 
+    let mut won_history = vec![];
     let mut won = [0; 100];
     let mut sum = [0; 100];
+    let mut values = [[[0; 5]; 5]; 100];
     let mut row_marked = [[[false; 5]; 5]; 100];
     let mut col_marked = [[[false; 5]; 5]; 100];
 
     for n in numbers {
       for (id, board) in boards.chunks(5).enumerate() {
-        sum[id] = 0;
-
         for (x, row) in board.iter().enumerate() {
           for (y, val) in row.iter().enumerate() {
+            values[id][x][y] = *val;
+
             if n == *val {
               row_marked[id][x][y] = true;
               col_marked[id][y][x] = true;
 
               if won[id] == 0 && (row_marked[id][x] == [true; 5] || col_marked[id][y] == [true; 5]) {
+                won_history.push(id);
                 won[id] = n;
+
+                for x in 0..5 {
+                  for y in 0..5 {
+                    if !row_marked[id][x][y] {
+                      sum[id] += values[id][x][y]
+                    }
+                  }
+                }
               }
-            } else {
-              sum[id] += *val;
             }
           }
         }
       }
     }
 
-    for id in 0..4 {
-      let n = won[id];
-      let s = sum[id];
+    won_history.remove(won_history.len() - 1);
+    let (first, last) = (*won_history.first().unwrap(), *won_history.last().unwrap());
 
-      println!("{n} {s} {}", n * s);
-    }
-
-    let part1 = || 49860;
-    let part2 = || 24628;
+    let part1 = || won[first] * sum[first];
+    let part2 = || won[last] * sum[last];
 
     answer!(part, part1, part2)
   }
