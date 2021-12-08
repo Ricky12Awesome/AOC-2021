cf5013dd483cc52214b28b14cb34e1f20c4032e1
Ricky12Awesome <rickygraveen@gmail.com>
2021-12-08 15:33:17 -0600

    Day 8 Part 2

--- src/day8.rs
+++ src/day8.rs
@@ -1,16 +1,5 @@
 use crate::day::*;
 
-//be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
-// edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
-// fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
-// fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
-// aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
-// fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
-// dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
-// bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
-// egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
-// gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
-
 day!(Day8);
 
 #[allow(unused)]
@@ -18,29 +7,51 @@ impl Day8 {
   fn day(part: Part) -> Answer<usize> {
     let segments = Self::INPUT
       .lines()
-      .map(|it| it.split('|'))
       .map(|it| {
-        it.map(|it| {
+        it.split('|').map(|it| {
           it.split(' ')
             .map(str::trim)
             .filter(not_empty)
             .map(str::chars)
-            .map(|it| it.fold(0u8, |n, c| n | 1 << (c as u8 - b'a')))
-            // .collect_vec()
+            .map(|chars| chars.fold(0u8, |n, c| n | 1 << (c as u8 - b'a')))
+            .collect_vec()
         })
       })
-      .map(|it| it.collect_arr::<2>())
-      .collect_vec();
+      .map(|it| it.collect_arr::<2>());
 
-    let mut p1 = 0;
+    let (mut p1, mut p2) = (0, 0);
 
     for [unique, output] in segments {
-      p1 += output.filter(|x| [2, 3, 4, 7].contains(&x.count_ones())).count()
+      p1 += output.iter().filter(|x| [2, 3, 4, 7].contains(&x.count_ones())).count();
+
+      let one = unique.iter().find(|it| it.count_ones() == 2).unwrap();
+      let four = unique.iter().find(|it| it.count_ones() == 4).unwrap();
+
+      p2 += output
+        .iter()
+        .map(
+          |it| match (it.count_ones(), (it & four).count_ones(), (it & one).count_ones()) {
+            (2, _, _) => 1,
+            (4, _, _) => 4,
+            (3, _, _) => 7,
+            (7, _, _) => 8,
+
+            (5, 2, _) => 2,
+            (5, 3, 1) => 5,
+            (5, 3, 2) => 3,
+
+            (6, 4, _) => 9,
+            (6, 3, 1) => 6,
+            (6, 3, 2) => 0,
+            _ => unreachable!(),
+          },
+        )
+        .rev()
+        .zip(std::iter::successors(Some(1usize), |n| n.checked_mul(10)))
+        .map(|(digit, n)| digit * n)
+        .sum::<usize>();
     }
 
-    let p1 = || p1;
-    let p2 = || 0;
-
-    answer!(part, p1, p2)
+    answer!(part, || p1, || p2)
   }
 }
