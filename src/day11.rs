9f446a4cfa79d4c9d9ee19187509b11a2e6a2863
Ricky12Awesome <rickygraveen@gmail.com>
2021-12-12 12:53:29 -0600

    Day 11

--- src/day11.rs
+++ src/day11.rs
@@ -1,11 +1,78 @@
 use crate::day::*;
 
-day!(Day11);
+day!(Day11, Some(1741), Some(440));
 
 impl Day11 {
   fn day(part: Part) -> Answer<usize> {
-    let p1 = || 0;
-    let p2 = || 0;
+    let grid = Self::INPUT
+      .lines()
+      .map(|it| it.chars().map(parse_char::<usize>).collect_arr::<10>())
+      .collect_arr::<10>();
+
+    let mut map = HashMap::new();
+
+    for y in 0..10 {
+      for x in 0..10 {
+        map.insert([y, x], grid[y][x]);
+      }
+    }
+
+    fn do_flash(grid: &mut HashMap<[usize; 2], usize>, y: usize, x: usize, flashed: &mut HashSet<[usize; 2]>) {
+      if flashed.contains(&[y, x]) {
+        return;
+      }
+
+      flashed.insert([y, x]);
+
+      for ([y, x], _) in grid.get_adjacent_at(y, x).into_iter().flatten() {
+        *grid.get_or_def_mut([y, x]) += 1;
+      }
+
+      for ([y, x], lvl) in grid.get_adjacent_at(y, x).into_iter().flatten() {
+        if lvl > 9 {
+          do_flash(grid, y, x, flashed);
+        }
+      }
+    }
+
+    fn do_step(grid: &mut HashMap<[usize; 2], usize>, flashed: &mut HashSet<[usize; 2]>) {
+      for (_, val) in grid.iter_mut() {
+        *val += 1;
+      }
+
+      for y in 0..10 {
+        for x in 0..10 {
+          if *grid.get(&[y, x]).unwrap() > 9 {
+            do_flash(grid, y, x, flashed);
+          }
+        }
+      }
+
+      for [y, x] in flashed.iter().copied() {
+        *grid.get_or_def_mut([y, x]) = 0;
+      }
+    }
+
+    let mut sum = 0;
+    let mut step = 0;
+
+    loop {
+      let mut set = HashSet::new();
+      do_step(&mut map, &mut set);
+
+      if step < 100 {
+        sum += set.len();
+      }
+
+      step += 1;
+
+      if map.values().sum::<usize>() == 0 {
+        break
+      }
+    }
+
+    let p1 = || sum;
+    let p2 = || step;
 
     answer!(part, p1, p2)
   }
--- src/util.rs
+++ src/util.rs
@@ -4,6 +4,7 @@ use std::collections::HashMap;
 use std::fmt::Debug;
 use std::hash::Hash;
 use std::num::{ParseFloatError, ParseIntError};
+use std::ops::Sub;
 use std::str::FromStr;
 
 pub use itertools::Itertools;
@@ -43,18 +44,54 @@ pub trait HashmapGetOrDefMut<K, V> {
   fn get_or_def_mut(&mut self, key: K) -> &mut V;
 }
 
+pub trait Array2DIndex<T> {
+  fn get_at(&self, y: usize, x: usize) -> Option<T>;
+
+  fn get_at_offset(&self, y: usize, x: usize, off_y: isize, off_x: isize) -> Option<([usize; 2], T)> {
+    let y = ((y as isize) + off_y).try_into().ok()?;
+    let x = ((x as isize) + off_x).try_into().ok()?;
+
+    self.get_at(y, x).map(|it| ([y, x], it))
+  }
+
+  fn get_adjacent_at(&self, y: usize, x: usize) -> [Option<([usize; 2], T)>; 8] {
+    [
+      self.get_at_offset(y, x, 1, 1),
+      self.get_at_offset(y, x, 1, 0),
+      self.get_at_offset(y, x, 0, 1),
+      self.get_at_offset(y, x, 1, -1),
+      self.get_at_offset(y, x, -1, 1),
+      self.get_at_offset(y, x, 0, -1),
+      self.get_at_offset(y, x, -1, 0),
+      self.get_at_offset(y, x, -1, -1),
+    ]
+  }
+}
+
 impl<I: Iterator> UnwrapNext for I {}
 
 impl<T: Debug, I: Iterator<Item = T>> CollectArr<T> for I {}
 
-impl <K: Hash + Eq, V> HashmapGetOr<K, V> for HashMap<K, V> {
+impl<K: Hash + Eq, V> HashmapGetOr<K, V> for HashMap<K, V> {
   fn get_or<'a>(&'a self, key: &'a K, val: &'a V) -> &'a V {
     self.get(key).unwrap_or(val)
   }
 }
 
-impl <K: Hash + Eq + Copy, V: Default> HashmapGetOrDefMut<K,V> for HashMap<K, V> {
+impl<K: Hash + Eq + Copy, V: Default> HashmapGetOrDefMut<K, V> for HashMap<K, V> {
   fn get_or_def_mut(&mut self, key: K) -> &mut V {
     self.entry(key).or_insert_with(|| V::default())
+  }
+}
+
+impl<T: Sized + Copy, const Y: usize, const X: usize> Array2DIndex<T> for [[T; Y]; X] {
+  fn get_at(&self, y: usize, x: usize) -> Option<T> {
+    self.get(y).and_then(|it| it.get(x).copied())
+  }
+}
+
+impl <T: Sized + Copy> Array2DIndex<T> for HashMap<[usize; 2], T> {
+  fn get_at(&self, y: usize, x: usize) -> Option<T> {
+    self.get(&[y, x]).copied()
   }
 }
